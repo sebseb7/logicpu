4362db6476d9674f4c7da80da71b2326eae35389
sebseb7 <sebgreenbus@gmail.com>
2022-07-12 18:43:23 +0200

    upd

--- demo_prime.asm
+++ demo_prime.asm
@@ -13,14 +13,14 @@ counter:
     .byte 0
 .org 0x8000 ; ROM
 start:
-	ld a,3 ; dividend/result
+    ld a,3 ; dividend/result
     st a,[dividend]
     st a,[result]
     ld a,2 ; divisor
     st a,[divisor]
     ld a,0 ; remainder
     st a,[remainder]
-	ld a,8 ; loopcount
+    ld a,8 ; loopcount
     st a,[counter]
 step:
     lsh [result]
@@ -30,40 +30,39 @@ step:
     sub [counter],1
     jmpc [end]
     ld a,[remainder]
-	sub [remainder],[divisor]
-	jmpcc [step]
+    sub [remainder],[divisor]
+    jmpcc [step]
     st a,[remainder]
    	jmp [step]
 
 end:
     sub [remainder_tmp],1
-	jmpc [next_div]
+    jmpc [next_div]
     sub [divisor],3
-	jmpc [next_div_prime]
+    jmpc [next_div_prime]
     add [divisor],2
     ld a,[dividend]
     st a,[result]
     ld a,0
     st a,[remainder]
-	ld a,8
+    ld a,8
     st a,[counter]
    	jmp [step]
 next_div_prime:
-	ld o,[dividend]
+    ld o,[dividend]
 	
 next_div:
-	ld a,[dividend]
-	add a,1
-	jmpc [repeat]
+    ld a,[dividend]
+    add a,1
+    jmpc [repeat]
     st a,[dividend]
     st a,[result]
-	sub a,1
+    sub a,1
     st a,[divisor]
     ld a,0
     st a,[remainder]
-	ld a,8
+    ld a,8
     st a,[counter]
-	sub a,a
    	jmp [step]
 
 repeat:
