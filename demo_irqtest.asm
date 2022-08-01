b7404f4b360861e3823e6fb3778522254cde3e97
sebseb7 <sebgreenbus@gmail.com>
2022-08-01 23:38:16 +0200

    upd

---
+++ demo_irqtest.asm
@@ -0,0 +1,26 @@
+.org 0x0000 ; ROM
+
+counter:
+	.byte 0
+
+.org 0x8000 ; ROM
+	ld a,BYTE0(handler)
+	push a
+	ld a,BYTE1(handler)
+	push a
+	sie
+	ld a,0
+	st a,[counter]
+loop:
+	jmp [loop]
+handler:
+	ld a,io
+	sub a,2
+	jmpc [less_than_one]
+	add [counter],1
+	ld o,[counter]
+	reti
+less_than_one:
+	sub [counter],1
+	ld o,[counter]
+	reti
